84300080781cecf92028ec7595b82abc12361e39
Alex Martin <martin.alex.t+github@gmail.com>
2015-03-03 17:34:20 -0500

    Add new recipe template & controller scaffold.
    No styling yet!

--- app/controllers/recipe/new.js
+++ app/controllers/recipe/new.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+ recipeTypes: ["Breakfast", "Lunch", "Dinner", "Dessert"],
+ ingredientUnits: ["Cups", "Item", "Ounces", "Tablespoons", "Teaspoons"],
+ actions: {
+    makeRecipe: function() {
+    //Grab all our inputs
+
+    //Wrap our input values in an object
+    
+    //Send that object to Parse
+    
+    //Transition to recipe view
+    
+    }
+  }
 });
+
