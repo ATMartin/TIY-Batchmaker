b95d9c82b10ed04039155a803907e1465e53c097
Alex Martin <martin.alex.t+github@gmail.com>
2015-03-05 02:12:27 -0500

    Incremental authentication update
    Switching temporarily to new feature branch to implement user.edit.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,4 +3,15 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   needs: ['user'],
   model: Ember.computed.alias('controllers.user.currentUser'),
+  actions: { 
+    logout: function() {
+      this.set('model', null);
+      Ember.$.ajaxSetup({
+        headers: {
+          "X-Parse-Session-Token": null
+        }  
+      });
+      console.log("Logged out!");
+    },
+  }
 });
