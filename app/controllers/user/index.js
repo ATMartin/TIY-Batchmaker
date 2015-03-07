4bd3cadd11f1694221c9a201e5b4667f5fd1c4af
Alex Martin <martin.alex.t+github@gmail.com>
2015-03-07 02:29:13 -0500

    Adding index page

--- app/controllers/user/index.js
+++ app/controllers/user/index.js
@@ -1,11 +1,25 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.ArrayController.extend({
   needs: ['user'],
   myUser: Ember.computed.alias('controllers.user.currentUser'),
+  recipesMine: function() {
+    var self = this;
+    return this.get('model').filter(function(recipe) {
+      return recipe.author === self.get('myUser').nickname;
+    });
+  }.property('model'),
+  recipesPublic: function() {
+    return this.get('model').filter(function(recipe) {
+      return recipe.isPublic;
+    });
+  }.property('model'),
   actions: {
     gotoView: function(model) {
       this.transitionToRoute("recipe.view", model);
+    },
+    gotoNew: function() {
+      this.transitionToRoute("recipe.new");
     }
   }
 });
