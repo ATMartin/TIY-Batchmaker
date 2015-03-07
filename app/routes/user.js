eec7fc2c1d4fac36567a44ddfda6995c515ee490
Alex Martin <martin.alex.t+github@gmail.com>
2015-03-07 01:42:59 -0500

    Add login lockout logic
    All user subroutes will now refuse to load if an active user is not logged in.
    Uses simple redirect to login route if no currentUser is found.

--- app/routes/user.js
+++ app/routes/user.js
@@ -4,4 +4,9 @@ export default Ember.Route.extend({
   model: function() {
     return {};
   },
+  afterModel: function() {
+    if (this.controllerFor('user').get("currentUser") === null) {
+      this.transitionTo("user.login");
+    }
+  }
 });
